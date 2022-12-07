afc71c02e858260ffc148fade42eabf64958840f
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-12-07 23:22:11 +0100

    frontend code

---
+++ app/app.py
@@ -0,0 +1,58 @@
+import os
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from tensorflow.keras.utils import load_img, img_to_array
+
+st.title('Tumor Classifier: Upload your X-Ray picture and find out whether the result is normal, benign or malignant')
+
+base_dir = os.path.dirname(__file__)
+cover_path = os.path.join(base_dir, 'xray_illustration.png')
+image = Image.open(cover_path)
+st.image(image, use_column_width=True)
+
+model = tf.keras.models.load_model('/home/amin/Documents/mini-project-cnn/model')
+
+def predict_class(image):
+
+	img = load_img(image, target_size=(128, 128), color_mode='grayscale')
+	x = img_to_array(img)
+	x = np.expand_dims(x, axis=0)
+	images = np.vstack([x])
+	images /= 255.0
+	prediction = model.predict(images)
+
+	return prediction
+
+
+
+
+st.info('Upload DDSM scan for getting result ...')
+file = st.file_uploader("Upload an image of an XRAY", type=["png"])
+
+
+
+if file is None:
+	# st.info('Upload DDSM scan for getting result ...')
+	pass
+
+else:
+	target_path = os.path.join(base_dir, 'tempDir', file.name)
+
+	with open(target_path, 'wb') as f:
+		f.write(file.getbuffer())
+
+	slot = st.empty()
+	slot.text('Running inference....')
+	target_image = Image.open(file)
+
+	st.image(target_image, caption="Input Image", width = 400)
+	pred = predict_class(target_path, model)
+	class_names = ['CAN', 'BEN', 'NOR']
+	result = class_names[np.argmax(pred)]
+	output = 'The image is a ' + result
+	slot.text('Done')
+	st.success(output)
+
+	os.remove(os.path.join(base_dir, 'tempDir', file.name))
